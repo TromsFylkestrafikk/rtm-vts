22f9f8682b187e2ed4343fdb4f66583b06520b2c
agu078 <agu078@uit>
2025-02-04 10:50:15 +0100

    functioning map sort of

--- DjangoBackEnd/map/views.py
+++ DjangoBackEnd/map/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def map(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('map.html')
+    return HttpResponse(template.render())
