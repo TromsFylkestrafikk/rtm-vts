afdc08b6870951762f5d43e14dad591c803f06da
agu078 <agu078@uit>
2025-02-05 12:15:27 +0100

    text for places, drawing for where traffic ferrys has been cancelled

--- DjangoBackEnd/map/urls.py
+++ DjangoBackEnd/map/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import location_geojson
 
 urlpatterns = [
     path('map/', views.map, name='map'),
+    path('api/locations/', location_geojson, name='location_geojson')
 ]
--- DjangoBackEnd/map/views.py
+++ DjangoBackEnd/map/views.py
@@ -1,7 +1,44 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from django.template import loader
+from .models import TransitInformation
+from pyproj import CRS, Transformer
 
 def map(request):
     template = loader.get_template('map.html')
     return HttpResponse(template.render())
+
+def location_geojson(request):
+    # Query all TransitInformation instances that have latitude and longitude
+    locations = TransitInformation.objects.filter(latitude__isnull=False, longitude__isnull=False)
+
+    crs_25833 = CRS('EPSG:25833')
+    crs_4326 = CRS('EPSG:4326')
+
+    transformer = Transformer.from_crs(crs_25833, crs_4326)
+    
+    lon,lat = transformer.transform(269079.634,6567685.167)
+
+    print(lon,lat)
+
+    features = []
+
+    for location in locations:
+        features.append({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [location.longitude, location.latitude]  # Using longitude, latitude
+            },
+            "properties": {
+                "name": location.situation_id,  # You can use any field here, like situation_id
+                "description": location.location_description  # If you want to add more details in the popup
+            }
+        })
+
+    geojson_data = {
+        "type": "FeatureCollection",
+        "features": features
+    }
+
+    return JsonResponse(geojson_data)
